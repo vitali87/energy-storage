cdb511ebec190a394674e651c2bbb82c9ebaec7e
vitali87 <eheva87@gmail.com>
2021-05-29 20:20:47 +0100

    model works for 24 hours ahead in distinct charge-discharge mode and saves results into csv file

--- energy_storage_model.py
+++ energy_storage_model.py
@@ -1,5 +1,8 @@
-from __future__ import division
 import pyomo.environ as pyo
+from pyomo.opt import SolverFactory
+import csv
+
+opt = pyo.SolverFactory('glpk')
 
 model = pyo.AbstractModel()
 
@@ -9,21 +12,54 @@ model.n = pyo.Param(within=pyo.NonNegativeIntegers)
 model.I = pyo.RangeSet(1, model.m)
 model.J = pyo.RangeSet(1, model.n)
 
-model.a = pyo.Param(model.I, model.J)
-model.b = pyo.Param(model.I)
-model.c = pyo.Param(model.J)
+model.p = pyo.Param(model.I, model.J)
+model.b = pyo.Param(model.J)
+model.cap = pyo.Param(default=2)
+model.dch_r = pyo.Param(default=1)
+model.ch_r = pyo.Param(default=1)
 
-# the next line declares a variable indexed by the set J
-model.x = pyo.Var(model.J, domain=pyo.NonNegativeReals)
+# Discharging variable
+model.x = pyo.Var(model.I,model.J, domain=pyo.NonNegativeReals)
+# Charging variable
+model.y = pyo.Var(model.I,model.J, domain=pyo.NonNegativeReals)
+# Volume variable
+model.v = pyo.Var(model.J, domain=pyo.NonNegativeReals)
+# Binary charging or discharging mode
+model.mode = pyo.Var(model.J, domain=pyo.Binary)
 
 def obj_expression(m):
-    return pyo.summation(m.c, m.x)
+    return (-pyo.summation(m.p, m.x) + pyo.summation(m.p, m.y))
 
-model.OBJ = pyo.Objective(rule=obj_expression)
+model.OBJ = pyo.Objective(rule=obj_expression,sense=pyo.minimize)
 
-def ax_constraint_rule(m, i):
+def cons_discharge_cap(m, j):
     # return the expression for the constraint for i
-    return sum(m.a[i,j] * m.x[j] for j in m.J) >= m.b[i]
+    return sum(m.x[i,j] for i in m.I) <= m.v[j]
+def cons_charge_cap(m, j):
+    return sum(m.y[i,j] for i in m.I) <= m.cap - m.v[j]
+def cons_discharge_rate(m,i,j):
+    return m.x[i,j] <= m.dch_r * m.mode[j]
+def cons_charge_rate(m,i,j):
+    return m.y[i,j] <= m.ch_r * (1 - m.mode[j])
+def cons_volume_change(m,j):
+    if j == 1:
+        return (m.v[j] == 0) # assuming storage volume is half full at the start
+    return  (m.v[j] ==  m.v[j-1] - sum(m.x[i,j] - m.y[i,j] for i in m.I))
 
-# the next line creates one constraint for each member of the set model.I
-model.AxbConstraint = pyo.Constraint(model.I, rule=ax_constraint_rule)
+model.DischargeCapConstraint = pyo.Constraint(model.J, rule=cons_discharge_cap)
+model.ChargeCapConstraint = pyo.Constraint(model.J, rule=cons_charge_cap)
+model.DischargeRateConstraint = pyo.Constraint(model.I,model.J, rule=cons_discharge_rate)
+model.ChargeRateConstraint = pyo.Constraint(model.I,model.J, rule=cons_charge_rate)
+model.VolumeChangeConstraint = pyo.Constraint(model.J, rule=cons_volume_change)
+
+instance = model.create_instance('data.dat')
+results = opt.solve(instance)
+instance.display()
+
+with open('result.csv','w') as f1:
+    for v in instance.component_objects(pyo.Var, active=True):
+        varobject = getattr(instance, str(v))
+        writer=csv.writer(f1, delimiter='\t',lineterminator='\n',)
+        for index in varobject:
+            row =  (v,index, varobject[index].value)
+            writer.writerow(row)        
