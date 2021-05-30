9a4ebd398df2dc843dde2a9c41400e622e0b82a1
vitali87 <eheva87@gmail.com>
2021-05-30 22:47:19 +0100

    Now the model optimises eeach day sequentially. Need to pass storage level at the end of each day and add charge/discharge efficiency and degradation rate.

--- energy_storage_model.py
+++ energy_storage_model.py
@@ -4,10 +4,14 @@ import csv
 import pandas as pd
 import numpy as np
 
-# Pre-processing part
-# Global parameters
+## Pre-processing part####
+# Global declarations
 N_hlf_hrs = 48
-t = range(1,N_hlf_hrs+1)
+t = range(1,N_hlf_hrs + 1)
+dt = pd.date_range(start='01/01/2018 00:00:00', 
+                   end='31/12/2020 23:30:00', 
+                   freq="0.5H")
+N_days = int(len(dt)/N_hlf_hrs)
 
 # Reading Market 1 and 3 data
 df12 = pd.read_excel(r'Copy of Market Data.xlsx',
@@ -15,11 +19,6 @@ df12 = pd.read_excel(r'Copy of Market Data.xlsx',
                     usecols = "B:C")
 y1 = df12['Market 1 Price [£/MWh]']
 y2 = df12['Market 2 Price [£/MWh]']
-y1 = y1[0:48]
-y2 = y2[0:48]
-
-M1data = {'I':1,'J':t,'p':y1} 
-M2data = {'I':2,'J':t,'p':y2} 
 
 # Putting Market 3 data in a half-hour format
 df3 = pd.read_excel(r'Copy of Market Data.xlsx',
@@ -27,128 +26,137 @@ df3 = pd.read_excel(r'Copy of Market Data.xlsx',
                     usecols = "A:B")
 x = np.array(df3["Market 3 Price [£/MWh]"])
 y3 = np.repeat(x, N_hlf_hrs, axis=0)
-y3 = y3[0:48]
 
-M3data = {'I':3,'J':t,'p':y3} 
+## Iterative optimisation: for each day ####
+for k in range(1, N_days):
+    y1_ = y1[(k-1)*48:k*48]
+    y2_ = y2[(k-1)*48:k*48]
+    M1data = {'I':1,'J':t,'p':y1_} 
+    M2data = {'I':2,'J':t,'p':y2_} 
+    
+    y3_ = y3[(k-1)*48:k*48]
+    M3data = {'I':3,'J':t,'p':y3_} 
+    
+    # Converting to Panda's df and row-binding
+    M1 = pd.DataFrame(M1data)
+    M2 = pd.DataFrame(M2data)
+    M3 = pd.DataFrame(M3data)
+    M = pd.concat([M1,M2,M3], ignore_index=True)
+    M.to_csv("data.csv", index=False)
+    
+    ### Modelling part ####
+    opt = pyo.SolverFactory('glpk')
+    
+    model = pyo.AbstractModel()
+    
+    model.m = pyo.Param(within=pyo.NonNegativeIntegers,default = 3)
+    model.n = pyo.Param(within=pyo.NonNegativeIntegers,default = N_hlf_hrs)
+    
+    model.I = pyo.RangeSet(1, model.m)
+    model.J = pyo.RangeSet(1, model.n)
 
-# COnverting to Panda's df and row-binding
-M1 = pd.DataFrame(M1data)
-M2 = pd.DataFrame(M2data)
-M3 = pd.DataFrame(M3data)
-M = pd.concat([M1,M2,M3], ignore_index=True)
-M.to_csv("data.csv", index=False)
+    model.p = pyo.Param(model.I,model.J) # Prices in Market 1 
 
-# Modelling part
-opt = pyo.SolverFactory('glpk')
+    model.cap = pyo.Param(default = 4) # Maximum volume of energy that the battery can store (MWh)
+    model.dch_r = pyo.Param(default = 1) # discharge rate possible for half hour is 1MW
+    model.ch_r = pyo.Param(default = 1) # charge rate possible for half hour is 1MW
 
-model = pyo.AbstractModel()
+    # Discharging variables for Market 1,2,3
+    model.x = pyo.Var(model.I,model.J, domain=pyo.NonNegativeReals)
 
-model.m = pyo.Param(within=pyo.NonNegativeIntegers,default = 3)
-model.n = pyo.Param(within=pyo.NonNegativeIntegers,default = N_hlf_hrs)
+    # Charging variables for Market 1 and 2. Charging variable for Market 3: equal constant charge rate for each half-hour period
+    model.y = pyo.Var(model.I, model.J, domain=pyo.NonNegativeReals)
 
-model.I = pyo.RangeSet(1, model.m)
-model.J = pyo.RangeSet(1, model.n)
+    # Volume variable
+    model.v = pyo.Var(model.J, domain=pyo.NonNegativeReals)
+    # Binary charging/discharging mode: 1 - discharging, 0 - charging
+    model.mode = pyo.Var(model.J, domain=pyo.Binary)
+    # Binary status of Market 3: 1 - used, 0 - not used
+    model.used = pyo.Var(domain=pyo.Binary)
+    # Binary charging/discharging mode of Market 3: 1 - discharging, 0 - charging
+    model.mode_3 = pyo.Var(domain=pyo.Binary)
 
-model.p = pyo.Param(model.I,model.J) # Prices in Market 1 
+    def obj_expression(m):
+        return (-pyo.summation(m.p, m.x) + pyo.summation(m.p, m.y))
+    model.OBJ = pyo.Objective(rule=obj_expression,sense=pyo.minimize)
 
-model.cap = pyo.Param(default = 4) # Maximum volume of energy that the battery can store (MWh)
-model.dch_r = pyo.Param(default = 1) # discharge rate possible for half hour is 1MW
-model.ch_r = pyo.Param(default = 1) # charge rate possible for half hour is 1MW
+    def cons_discharge_cap(m, j):
+        return sum(m.x[i,j] for i in m.I) <= m.v[j]
+    model.DischargeCapConstraint = pyo.Constraint(model.J, rule=cons_discharge_cap)
 
-# Discharging variables for Market 1,2,3
-model.x = pyo.Var(model.I,model.J, domain=pyo.NonNegativeReals)
+    def cons_charge_cap(m, j):
+        return sum(m.y[i,j] for i in m.I) <= m.cap - m.v[j]
+    model.ChargeCapConstraint = pyo.Constraint(model.J, rule=cons_charge_cap)
 
-# Charging variables for Market 1 and 2. Charging variable for Market 3: equal constant charge rate for each half-hour period
-model.y = pyo.Var(model.I, model.J, domain=pyo.NonNegativeReals)
+    def cons_discharge_rate_combined(m,j):
+        return sum(m.x[i,j] for i in m.I) <= m.dch_r * m.mode[j]
+    model.DischargeRateCombConstraint = pyo.Constraint(model.J, rule=cons_discharge_rate_combined)
 
-# Volume variable
-model.v = pyo.Var(model.J, domain=pyo.NonNegativeReals)
-# Binary charging/discharging mode: 1 - discharging, 0 - charging
-model.mode = pyo.Var(model.J, domain=pyo.Binary)
-# Binary status of Market 3: 1 - used, 0 - not used
-model.used = pyo.Var(domain=pyo.Binary)
-# Binary charging/discharging mode of Market 3: 1 - discharging, 0 - charging
-model.mode_3 = pyo.Var(domain=pyo.Binary)
+    def cons_charge_rate_combined(m,j):
+        return sum(m.y[i,j] for i in m.I) <= m.ch_r * (1 - m.mode[j])
+    model.ChargeRateCombConstraint = pyo.Constraint(model.J, rule=cons_charge_rate_combined)
 
-def obj_expression(m):
-    return (-pyo.summation(m.p, m.x) + pyo.summation(m.p, m.y))
-model.OBJ = pyo.Objective(rule=obj_expression,sense=pyo.minimize)
+    def cons_volume_change(m,j):
+        if j == 1:
+            return (m.v[j] == m.cap/2) # assuming storage volume is half full at the start
+        return  (m.v[j] ==  m.v[j-1] - sum(m.x[i,j] - m.y[i,j] for i in m.I))
+    model.VolumeChangeConstraint = pyo.Constraint(model.J, rule=cons_volume_change)
 
-def cons_discharge_cap(m, j):
-    return sum(m.x[i,j] for i in m.I) <= m.v[j]
-model.DischargeCapConstraint = pyo.Constraint(model.J, rule=cons_discharge_cap)
+    def cons_discharge_M3(m):
+        return sum(m.x[3,j] for j in m.J) <= m.mode_3 * m.cap
+    model.DischargeM3Constraint = pyo.Constraint(rule=cons_discharge_M3)
 
-def cons_charge_cap(m, j):
-    return sum(m.y[i,j] for i in m.I) <= m.cap - m.v[j]
-model.ChargeCapConstraint = pyo.Constraint(model.J, rule=cons_charge_cap)
+    def cons_discharge_sum_M3(m,j):
+        if j == 1:
+            return pyo.Constraint.Skip
+        return m.x[3,j-1] == m.x[3,j]
+    model.DischargeSumM3Constraint = pyo.Constraint(model.J, rule=cons_discharge_sum_M3)
 
-def cons_discharge_rate_combined(m,j):
-    return sum(m.x[i,j] for i in m.I) <= m.dch_r * m.mode[j]
-model.DischargeRateCombConstraint = pyo.Constraint(model.J, rule=cons_discharge_rate_combined)
+    def cons_charge_M3(m):
+        return sum(m.y[3,j] for j in m.J) <= (1 - m.mode_3) * m.cap
+    model.ChargeM3Constraint = pyo.Constraint(rule=cons_charge_M3)
 
-def cons_charge_rate_combined(m,j):
-    return sum(m.y[i,j] for i in m.I) <= m.ch_r * (1 - m.mode[j])
-model.ChargeRateCombConstraint = pyo.Constraint(model.J, rule=cons_charge_rate_combined)
+    def cons_charge_sum_M3(m,j):
+        if j == 1:
+            return pyo.Constraint.Skip
+        return m.y[3,j-1] == m.y[3,j]
+    model.ChargeSumM3Constraint = pyo.Constraint(model.J, rule=cons_charge_sum_M3)
 
-def cons_volume_change(m,j):
-    if j == 1:
-        return (m.v[j] == m.cap/2) # assuming storage volume is half full at the start
-    return  (m.v[j] ==  m.v[j-1] - sum(m.x[i,j] - m.y[i,j] for i in m.I))
-model.VolumeChangeConstraint = pyo.Constraint(model.J, rule=cons_volume_change)
+    def cons_M3_ch_used(m,j):
+        return m.y[3,j]  <= m.used * m.cap
+    model.M3ChUsedConstraint = pyo.Constraint(model.J, rule=cons_M3_ch_used)
 
-def cons_discharge_M3(m):
-    return sum(m.x[3,j] for j in m.J) <= m.mode_3 * m.cap
-model.DischargeM3Constraint = pyo.Constraint(rule=cons_discharge_M3)
+    def cons_M3_dch_used(m,j):
+        return m.x[3,j]  <= m.used * m.cap
+    model.M3DchUsedConstraint = pyo.Constraint(model.J, rule=cons_M3_dch_used)
 
-def cons_discharge_sum_M3(m,j):
-    if j == 1:
-        return pyo.Constraint.Skip
-    return m.x[3,j-1] == m.x[3,j]
-model.DischargeSumM3Constraint = pyo.Constraint(model.J, rule=cons_discharge_sum_M3)
+    def cons_mode_relation1(m,j):
+        return m.mode[j] - m.mode_3 <= 1 - m.used
+    model.ModeRelation1Constraint = pyo.Constraint(model.J, rule=cons_mode_relation1)
 
-def cons_charge_M3(m):
-    return sum(m.y[3,j] for j in m.J) <= (1 - m.mode_3) * m.cap
-model.ChargeM3Constraint = pyo.Constraint(rule=cons_charge_M3)
+    def cons_mode_relation2(m,j):
+        return m.mode[j] - m.mode_3 >= m.used - 1
+    model.ModeRelation2Constraint = pyo.Constraint(model.J, rule=cons_mode_relation2)
 
-def cons_charge_sum_M3(m,j):
-    if j == 1:
-        return pyo.Constraint.Skip
-    return m.y[3,j-1] == m.y[3,j]
-model.ChargeSumM3Constraint = pyo.Constraint(model.J, rule=cons_charge_sum_M3)
+    data = pyo.DataPortal()
+    data.load(filename = 'data.csv',  
+                param = model.p)
 
-def cons_M3_ch_used(m,j):
-    return m.y[3,j]  <= m.used * m.cap
-model.M3ChUsedConstraint = pyo.Constraint(model.J, rule=cons_M3_ch_used)
+    instance = model.create_instance(data)
+    results = opt.solve(instance)
 
-def cons_M3_dch_used(m,j):
-    return m.x[3,j]  <= m.used * m.cap
-model.M3DchUsedConstraint = pyo.Constraint(model.J, rule=cons_M3_dch_used)
+    ### Output generation part #####
+    with open('result.csv','a') as f1:
+        for v in instance.component_objects(pyo.Var, active = True):
+            varobject = getattr(instance, str(v))
+            writer = csv.writer(f1, 
+                                delimiter = '\t',
+                                lineterminator = '\n',)
+            for index in varobject:
+                row =  (v,
+                        index, 
+                        varobject[index].value)
+                writer.writerow(row)  
+    print("Day ",k,"/",N_days," is finished")      
 
-def cons_mode_relation1(m,j):
-    return m.mode[j] - m.mode_3 <= 1 - m.used
-model.ModeRelation1Constraint = pyo.Constraint(model.J, rule=cons_mode_relation1)
-
-def cons_mode_relation2(m,j):
-    return m.mode[j] - m.mode_3 >= m.used - 1
-model.ModeRelation2Constraint = pyo.Constraint(model.J, rule=cons_mode_relation2)
-
-data = pyo.DataPortal()
-
-data.load(filename = 'data.csv',  
-            param = model.p)
-
-instance = model.create_instance(data)
-results = opt.solve(instance)
-
-# Output generation part
-with open('result.csv','w') as f1:
-    for v in instance.component_objects(pyo.Var, active = True):
-        varobject = getattr(instance, str(v))
-        writer = csv.writer(f1, 
-                            delimiter = '\t',
-                            lineterminator = '\n',)
-        for index in varobject:
-            row =  (v,
-                    index, 
-                    varobject[index].value)
-            writer.writerow(row)        
+## Post-processing ####
